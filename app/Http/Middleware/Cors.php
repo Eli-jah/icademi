60a56d018c744ac44f8c2b05657281fbd2514d9c
Elijah Wang <1198331919@qq.com>
2021-11-17 21:07:05 +0800

    fix: nginx_app.conf updated for CORS issue +1.

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET,POST,OPTIONS,DELETE,PUT,PATCH');
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Headers: DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization');
+
+        return $next($request);
+    }
+}
