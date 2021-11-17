4d95dab038ea00a25f30ea0a149252d067c5554e
Elijah Wang <1198331919@qq.com>
2021-11-17 23:12:42 +0800

    fix: nginx_app.conf updated for CORS issue +7.

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -22,21 +22,21 @@ class Cors
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Allow-Headers: Access-Control-Allow-Origin,Access-Control-Allow-Methods,DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization');
 
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'GET,POST,OPTIONS,DELETE,PUT,PATCH',
-            'Access-Control-Allow-Headers' => 'Access-Control-Allow-Origin,Access-Control-Allow-Methods,DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization',
-        ];
+        // $headers = [
+        //     'Access-Control-Allow-Origin' => '*',
+        //     'Access-Control-Allow-Methods' => 'GET,POST,OPTIONS,DELETE,PUT,PATCH',
+        //     'Access-Control-Allow-Headers' => 'Access-Control-Allow-Origin,Access-Control-Allow-Methods,DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization',
+        // ];
 
-        if ($request->getMethod() == "OPTIONS") {
-            // return Response::make('OK', 200, $headers);
-            return response()->make('OK', 200, $headers);
-        }
+        // if ($request->getMethod() == "OPTIONS") {
+        //     // return Response::make('OK', 200, $headers);
+        //     return response()->make('OK', 200, $headers);
+        // }
 
         $response = $next($request);
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
+        // foreach ($headers as $key => $value) {
+        //     $response->header($key, $value);
+        // }
 
         return $response;
     }
