f3a40ab5151d8d3254de6144b0cefa34b713d77e
Elijah Wang <1198331919@qq.com>
2021-11-22 10:37:17 +0800

    fix: git ignore some files for deployment +3.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class IndexController extends Controller
 {
@@ -19,9 +20,9 @@ class IndexController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
         return view('welcome');
     }
