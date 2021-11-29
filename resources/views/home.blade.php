6a8bcd9c3d0afb776c99ac84531f40388e353d5f
Elijah Wang <1198331919@qq.com>
2021-11-29 18:33:52 +0800

    feature(home): a better home page for display +15.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,75 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
 
-@section('content')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/web-app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#app-navbar-collapse" aria-expanded="false">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"
+                           aria-haspopup="true" v-pre>
+                            {{ $user->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    General Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -103,50 +172,56 @@
         </div>
     </div>
     <div style="display: none;" data-token="{{ $token }}" id="token"></div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.js"
-            integrity="sha512-RT3IJsuoHZ2waemM8ccCUlPNdUuOn8dJCH46N3H2uZoY7swMn1Yn7s56SsE2UBMpjpndeZ91hm87TP1oU6ANjQ=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        (function () {
-            const teachers = document.getElementsByClassName('teacher-is-followed');
-            const token = document.getElementById('token').getAttribute('data-token');
-            const axiosInstance = axios.create({
-                baseURL: 'https://icademi.herokuapp.com/api/',
-                timeout: 9000,
-                headers: {
-                    Accept: "application/json",
-                    Authorization: 'Bearer ' + token,
-                },
-            });
-            for (let teacher in teachers) {
-                let teacher_id = teacher.getAttribute('data-teacher-id');
-                let is_followed = teacher.getAttribute('data-is-followed');
-                teacher.onclick = function () {
-                    if (is_followed === 'yes') {
-                        axiosInstance
-                            .post("student/unfollow_teacher", {
-                                teacher_id: teacher_id,
-                            })
-                            .then(function (response) {
-                                console.log(response);
-                            })
-                            .catch(function (error) {
-                                console.log(error);
-                            });
-                    } else {
-                        axiosInstance
-                            .post("student/follow_teacher", {
-                                teacher_id: teacher_id,
-                            })
-                            .then(function (response) {
-                                console.log(response);
-                            })
-                            .catch(function (error) {
-                                console.log(error);
-                            });
-                    }
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/web-app.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.js"
+        integrity="sha512-RT3IJsuoHZ2waemM8ccCUlPNdUuOn8dJCH46N3H2uZoY7swMn1Yn7s56SsE2UBMpjpndeZ91hm87TP1oU6ANjQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    (function () {
+        const teachers = document.getElementsByClassName('teacher-is-followed');
+        const token = document.getElementById('token').getAttribute('data-token');
+        const axiosInstance = axios.create({
+            baseURL: 'https://icademi.herokuapp.com/api/',
+            timeout: 9000,
+            headers: {
+                Accept: "application/json",
+                Authorization: 'Bearer ' + token,
+            },
+        });
+        for (let teacher in teachers) {
+            console.log(teacher);
+            let teacher_id = teacher.getAttribute('data-teacher-id');
+            let is_followed = teacher.getAttribute('data-is-followed');
+            teacher.onclick = function () {
+                if (is_followed === 'yes') {
+                    axiosInstance
+                        .post("student/unfollow_teacher", {
+                            teacher_id: teacher_id,
+                        })
+                        .then(function (response) {
+                            console.log(response);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                } else {
+                    axiosInstance
+                        .post("student/follow_teacher", {
+                            teacher_id: teacher_id,
+                        })
+                        .then(function (response) {
+                            console.log(response);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
                 }
             }
-        })();
-    </script>
-@endsection
+        }
+    })();
+</script>
+</body>
+</html>
